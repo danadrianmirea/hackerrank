43185f124fa740f072dd55ec41f11710b04541c8
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-21 09:21:01 +0200

    modified:   code/main.c

--- code/main.c
+++ code/main.c
@@ -3,5 +3,17 @@
 #include <math.h>
 
 int main() {
+
+    int n = 81923;
+    int sum = 0;
+    
+    while (n)
+    {
+        sum += n%10;
+        n = n/10;
+    }
+
+    printf("%d", sum);
+
     return 0;
 }
