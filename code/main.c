cec5f878dc10cbc9e59ee72825088ba7a340234e
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-22 10:03:20 +0200

    modified:   code/main.c

--- code/main.c
+++ code/main.c
@@ -4,5 +4,22 @@
 
 int main()
 {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n;
+    int sum = 0;
+    scanf("%d\n", &n);
+    int *arr = (int*)malloc(n*sizeof(int));
+    
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d ", &arr[i]);
+        sum += arr[i];
+    }
+    
+    printf("%d\n", sum);
+    
+    free(arr);
+    
+
     return 0;
 }
