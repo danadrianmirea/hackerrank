215bbb184efda8f2702cdfcf8f12961d44b55613
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-22 13:38:55 +0200

    modified:   code/main.c

--- code/main.c
+++ code/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+void swap(int*a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
 
 int main()
 {
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) 
+    {
+        scanf("%d", arr + i);
+    }
+
+    /* Write the logic to reverse the array. */
+    for(i = 0; i < num/2; i++) 
+    {
+        swap(&arr[i], &arr[num-i-1]);
+    }
+
+    for(i = 0; i < num; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
     return 0;
 }
