bd08d936cdc7ada3a2ee005fb110461c389e0954
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-28 15:32:58 +0200

    Fix segf - strtok doesn't work with string literal

--- code/split_string_in_tokens.h
+++ code/split_string_in_tokens.h
@@ -3,13 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
-    void splitStringInTokens(char* s)
+    void splitStringInTokens(const char* s)
     {
+        char* str = malloc(strlen(s) + 1);
+        strcpy(str, s);
+
         // split into tokens and print them
-        char* p = strtok(s, " ");
+        char* p = strtok(str, " ");
         while (p)
         {
             printf("%s\n", p);
             p = strtok(NULL, " ");
         }
+
+        free(str);
     }
