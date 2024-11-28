ed76a8ea7ffeff27b3b1c87db1a9ce961185b3b4
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-28 15:31:10 +0200

    test code

--- code/main.c
+++ code/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <string.h>
+#include "split_string_in_tokens.h"
 
 int main()
 {
+    splitStringInTokens("Some test string");
     return 0;
 }
--- code/split_string_in_tokens.h
+++ code/split_string_in_tokens.h
@@ -3,15 +3,13 @@
 #include <stdio.h>
 #include <string.h>
 
-void splitStringInTokens(char* s)
-{
-    if (s == nullptr) return;
-
-    // split into tokens and print them
-    char* p = strtok(s, " ");
-    while (p)
+    void splitStringInTokens(char* s)
     {
-        printf("%s\n", p);
-        p = strtok(NULL, " ");
+        // split into tokens and print them
+        char* p = strtok(s, " ");
+        while (p)
+        {
+            printf("%s\n", p);
+            p = strtok(NULL, " ");
+        }
     }
-}
