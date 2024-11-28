5a2a81195a645ab7a8471ea759d1bbc61029e6d8
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-28 15:23:31 +0200

    modified:   code/main.c

--- code/main.c
+++ code/main.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <string.h>
+
 int main()
 {
+    char s[] = "This is an example string";
+
+    // split into tokens and print them
+    char* p = strtok(s, " ");
+    while (p)
+    {
+        printf ("%s\n",p);
+        p = strtok (NULL, " ");
+    }
     return 0;
 }
