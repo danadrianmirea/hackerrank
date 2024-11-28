e9b1d0b08699c9a271a52eb72e0e40b402d382e1
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-29 01:52:17 +0200

    modified:   code/main.c

--- code/main.c
+++ code/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    char s[1024] = "a11472o5t6";
+    //scanf("%s\n", s);
+    
+    int freqMap[10] = {0};
+    for(int i=0; i<strlen(s); i++)
+    {
+        if('0' <= s[i] && s[i] <= '9')
+        {
+            freqMap[s[i]-'0']++;
+        }
+    }
+    
+    for(int i=0; i<10; i++)
+    {
+        printf("%d ", freqMap[i]);
+    }
+
     return 0;
 }
