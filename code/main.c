b86558bbd41ef5fbb22fa8216934c45b7643f6f7
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-29 03:25:49 +0200

    add freqmap

--- code/main.c
+++ code/main.c
@@ -4,22 +4,6 @@
 
 int main()
 {
-    char s[1024] = "a11472o5t6";
-    //scanf("%s\n", s);
-    
-    int freqMap[10] = {0};
-    for(int i=0; i<strlen(s); i++)
-    {
-        if('0' <= s[i] && s[i] <= '9')
-        {
-            freqMap[s[i]-'0']++;
-        }
-    }
-    
-    for(int i=0; i<10; i++)
-    {
-        printf("%d ", freqMap[i]);
-    }
 
     return 0;
 }
