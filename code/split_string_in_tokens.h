79e35f7ec68438b8e3d73fea61c5dfb76baa1cec
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-29 03:24:02 +0200

    modified:   code/split_string_in_tokens.h

--- code/split_string_in_tokens.h
+++ code/split_string_in_tokens.h
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
-    void splitStringInTokens(const char* s)
+void splitStringInTokens(const char* s)
+{
+    char* str = malloc(strlen(s) + 1);
+    strcpy(str, s);
+
+    // split into tokens and print them
+    char* p = strtok(str, " ");
+    while (p)
     {
-        char* str = malloc(strlen(s) + 1);
-        strcpy(str, s);
-
-        // split into tokens and print them
-        char* p = strtok(str, " ");
-        while (p)
-        {
-            printf("%s\n", p);
-            p = strtok(NULL, " ");
-        }
-
-        free(str);
+        printf("%s\n", p);
+        p = strtok(NULL, " ");
     }
+
+    free(str);
+}
