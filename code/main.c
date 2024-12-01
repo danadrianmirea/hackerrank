46525a975e0503e8ea7bce7363bf2d7cb78850d5
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-02 00:25:48 +0200

    list stuff

--- code/main.c
+++ code/main.c
@@ -2,7 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct List List;
+
+struct List
+{
+    int data;
+    List* next;
+};
+
+void AllocNode(List* node)
+{
+    node = (List*)malloc(sizeof(List));
+}
+
+void FreeNode(List* node)
+{
+    free(node);
+}
+
+
 int main()
 {
+    List* node;
+    AllocNode(node);
+
+    FreeNode(node);
     return 0;
 }
