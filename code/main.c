043b6a3701bc466e1331ae1c7485ed11b68ebbab
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-02 00:49:36 +0200

    some stuff

--- code/main.c
+++ code/main.c
@@ -20,11 +20,29 @@ void FreeNode(List** node)
     free(*node);
 }
 
+void SetData(List** node, int value)
+{
+    (*node)->data = value;
+}
+
+void PrintData(List* node)
+{
+    printf("Node value is: %d\n", node->data);
+}
+
+void PrintLink(List* node)
+{
+    printf("Link value is: %x\n", node->next);
+}
 
 int main()
 {
     List* node;
     AllocNode(&node);
+
+    SetData(&node, 3);
+    PrintData(node);
+    PrintLink(node);
 
     FreeNode(&node);
     return 0;
