b42a303194673885c8c7a946170985e541776933
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-05 22:33:38 +0200

    new file:   code/comparingnumbers.sh

---
+++ code/comparingnumbers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt the user for two integers
+read -p "Enter the first integer (x): " x
+read -p "Enter the second integer (y): " y
+
+if [ "$x" -gt "$y" ]; then
+    echo "X is greater than Y"
+else if [ "$x" -eq "$y" ]; then 
+    echo "X is equal to Y"
+else
+    echo "X is less than Y"
+fi
