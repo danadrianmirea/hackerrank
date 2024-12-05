9b3ebf13560e9d94be01ea729aa5a06c3c6941af
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-05 22:55:36 +0200

    fix script (bash is stupid, elif is stupid)

--- code/compnumbers.sh
+++ code/compnumbers.sh
@@ -6,7 +6,7 @@ read -p "Enter the second integer (y): " y
 
 if [ "$x" -gt "$y" ]; then
     echo "X is greater than Y"
-else if [ "$x" -eq "$y" ]; then 
+elif [ "$x" -eq "$y" ]; then 
     echo "X is equal to Y"
 else
     echo "X is less than Y"
