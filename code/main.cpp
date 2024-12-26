94e0f2ea371c5ff15a2f48ffd6ba2fae39809f59
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-26 02:55:56 +0200

    cleanup main

--- code/main.cpp
+++ code/main.cpp
@@ -4,55 +4,7 @@
 using namespace std;
 
 
-string decryptPassword(string s) 
-{
-    string d=s;
-    
-    bool lastUpper = false;
-    
-    for(int i=0; i<d.size(); i++)
-    {
-        if(isupper(d[i]))
-        {
-            lastUpper = true;
-            continue;
-        }
-     
-        if(isdigit(d[i]) && d[i] != '0')
-        {
-            for(int j=d.size()-1; j>i; j--)
-            {
-                if(d[j] == '0')
-                {
-                    swap(d[i], d[j]);
-                    break;
-                }
-            }
-        }      
-        else if(lastUpper == true && islower(d[i]))
-        {
-            swap(d[i-1], d[i]);
-        }
-    }
-    
-    d.erase(std::remove(d.begin(), d.end(), '0'), d.end());
-    d.erase(std::remove(d.begin(), d.end(), '*'), d.end());
-  
-    return d;
-}
-
-
 int main()
 {
-    //ofstream fout(getenv("OUTPUT_PATH"));
-
-    string s = "51Pa*0Lp*0e";
-   
-
-    string result = decryptPassword(s);
-
-    cout << result << "\n";
-
-
     return 0;
 }
